11a978bd1d41aaf90315da0629cc6b84443cb31e
etaisella <74829220+etaisella@users.noreply.github.com>
2022-03-13 18:29:59 +0200

    uploading additional setups

---
+++ pytracking/parameter/kalmanBased/standard_75.py
@@ -0,0 +1,17 @@
+from pytracking.utils import TrackerParams
+from pytracking.features.net_wrappers import NetWithBackbone
+
+def parameters():
+    params = TrackerParams()
+
+    params.num_particles = 140
+    params.x_rand_scale = 1.25
+    params.y_rand_scale = 1.25
+    params.x_box_rand_scale = 0.4
+    params.y_box_rand_scale = 0.8
+    params.min_box_width = 5
+    params.min_box_height = 5
+    params.gt_update_interval = 9999
+    params.lambda_param = 0.75
+
+    return params
